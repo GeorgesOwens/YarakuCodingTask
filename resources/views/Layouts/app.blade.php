bf331b33ddad24ff7f42d7b29783a20cc72c40b8
Georges Owens <georges.owens@gmail.com>
2020-06-18 19:13:48 +0100

    Layout takes title from app name config

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+    <title>{{config('app.name')}}</title>
 
     </head>
     <body>
-        
+
         @yield('content')
 
     </body>
