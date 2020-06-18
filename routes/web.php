dcab9ff078e789a512dd38087846830eeff45c52
Georges Owens <georges.owens@gmail.com>
2020-06-18 19:00:29 +0100

    added routes for search and add pages

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.index');
 });
+
+Route::get('/search', function () {
+    return view('pages.search');
+});
+
+Route::get('/add', function () {
+    return view('pages.add');
+});
+
