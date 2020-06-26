61630e7a6d0972b978a622d272619314769705e9
Georges Owens <georges.owens@gmail.com>
2020-06-26 18:57:51 +0100

    Added a navbar to the main layout

---
+++ resources/views/Inc/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <a class="navbar-brand" href="/">BookRepo</a>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/search">Search</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/add">Add Book</a>
+      </li>
+    </ul>
+</nav>
--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -3,12 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name')}}</title>
-
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>{{config('app.name')}}</title>
     </head>
     <body>
-
-        @yield('content')
+        @include('Inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
 
     </body>
 </html>
